d8aced04584d54dedcde160f3f8cd3bbfe4abbc0
Abraham Maleko <abrahammaleko@gmail.com>
2023-07-12 11:38:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API endpoints will be defined here
+
+const port = 3000; // choose any port you prefer
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// GET endpoint
+app.get('/api/merchant-info', (req, res) => {
+  
+    const label = 'Abraham Maleko (The Developer)';
+    const icon = 'https://exiledapes.academy/wp-content/uploads/2021/09/X_share.png';
+  
+    res.status(200).json({
+      label,
+      icon,
+    });
+});
