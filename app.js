d7b656d14a066aed7e30410bfdb0b4d1130853a9
Abraham Maleko <abrahammaleko@gmail.com>
2023-07-12 17:24:13 +0300

    solana post request

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import { clusterApiUrl, Connection, Keypair, PublicKey, Transaction } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
+import { createTransferCheckedInstruction, getAccount, getAssociatedTokenAddress, getMint } from '@solana/spl-token';
+import { TEN } from '@solana/pay';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -25,3 +30,41 @@ app.get('/api/merchant', (req, res) => {
       icon,
     });
 });
+
+const splToken = new PublicKey(process.env.USDC_MINT);
+const MERCHANT_WALLET = new PublicKey("CVmz887tvi36wB2Jw7aYAHfenB2KJk5MHgaNV6xEjpEr");
+
+app.post('/api/merchant',async(request,response)=>{
+
+   // Account provided in the transaction request body by the wallet.
+   const accountField = request.body?.account;
+   if (!accountField) throw new Error('missing account');
+   
+   const sender = new PublicKey(accountField);
+
+    // create spl transfer instruction
+    // const splTransferIx = await createSplTransferIx(sender, connection);
+
+    // create the transaction
+    const transaction = new Transaction(
+      SystemProgram.transfer({
+        fromPubkey: sender,
+        toPubkey: MERCHANT_WALLET,
+        lamports: 1000000000,
+      })
+    );
+
+      // Serialize and return the unsigned transaction.
+      const serializedTransaction = transaction.serialize({
+        verifySignatures: false,
+        requireAllSignatures: false,
+      });
+
+      const base64Transaction = serializedTransaction.toString('base64');
+      const message = 'Thank you for your purchase';
+
+      response.status(200).send({ transaction: base64Transaction, message });
+
+
+});
+
